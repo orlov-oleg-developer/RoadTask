1e97f4aa785ac7bcfce16da1d58d75ead1d0cd1d
Orlov Oleg <Dark.eagle217@yandex.ru>
2022-05-17 23:34:50 +0300

    Кнопка добавляет текстовый элемент

---
+++ script.js
@@ -0,0 +1,9 @@
+let div = document.createElement('div');
+
+function newTitle() {
+     div.className = "page-title";
+     div.innerHTML = "<div>Всем привет!</div>";
+     
+     document.body.append(div);
+}
+
